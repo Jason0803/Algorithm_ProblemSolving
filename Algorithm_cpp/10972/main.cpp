159cdaa205a93560c76776b2b45f8736369e41fa
Jason <c3171700@uon.edu.au>
2018-01-14 23:16:40 +0900

    commits anyway

---
+++ Algorithm_cpp/10972/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n;
+    scanf("%d", &n);
+    vector<int> a(n);
+    for(int i=0; i<n; i++)
+        scanf("%d", &a[i]);
+    if(next_permutation(a.begin(), a.end())){
+        for(int i=0; i<n; i++)
+            printf("%d ", a[i]);
+    } else printf("%d", -1);
+    return 0;
+}
