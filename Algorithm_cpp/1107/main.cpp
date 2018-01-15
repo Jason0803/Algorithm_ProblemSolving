e56425188f4d77009b9751d7d29dc2be81f52f4f
Jason <c3171700@uon.edu.au>
2018-01-15 21:12:31 +0900

    1107 - 시간초과 ㅠㅠ

---
+++ Algorithm_cpp/1107/main.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+int buttons[10];
+
+bool ispossible(int x) {
+    while(x > 0) {
+        if(buttons[x%10] == 0)
+            return false;
+        x /= 10;
+    }
+    return true;
+}
+int main() {
+    int n, m, input;
+    
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    for(int i=0; i<10; i++){
+        buttons[i] = 1;
+    }
+    
+    for(int i=0; i<m; i++) {
+        scanf("%d", &input);
+        buttons[input] = 0;
+    }
+    
+    int count = 0;
+    
+    // 1. 번호 + 직접가는것
+    int temp, dist;
+    // 1-1. 감소 중에서 찾음
+    temp = n;
+    while(true){
+        if(ispossible(temp)) break;
+        else temp--;
+    }
+    dist = n - temp;
+    
+    // 1.2 증가 중에서 찾음
+    temp = n;
+    while(true) {
+        if(ispossible(temp)) break;
+            else temp++;
+    }
+    if(dist > (temp - n))
+        dist = temp -n;
+    
+    temp = n;
+    while(temp){
+        temp /= 10;
+        count++;
+    }
+    
+    // 2. 100 번부터 차례로 하는것과 비교
+    if(n < 100) {
+        if(dist > (100-n))
+            dist = 100 - n;
+    } else if(n > 100) {
+        if(dist > (n-100))
+            dist = n - 100;
+    } else {
+        dist = 0;
+        count = 0;
+    }
+    
+    count += dist;
+    printf("%d\n", count);
+    
+    return 0;
+}
