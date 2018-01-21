50fd278bf0a2a871c1d30485f36f6a6e802b2b64
Jason <c3171700@uon.edu.au>
2018-01-21 14:58:01 +0900

    그대로출력 > 출력초과 ㅠㅠ

---
+++ Algorithm_cpp/11718/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstring>
+int main(){
+    char input[102];
+
+    while(scanf("%s",input) != 0){
+        printf("%s",input);
+        puts("");
+    }
+    return 0;
+}
