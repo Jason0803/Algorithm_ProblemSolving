d4cabaeca45f9d94e2987d28a0d20df9646f7e88
Jason <c3171700@uon.edu.au>
2018-02-01 00:07:22 +0900

    commits

--- Algorithm_cpp/1182/main.cpp
+++ Algorithm_cpp/1182/main.cpp
@@ -1,20 +1,29 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
-int main() {
-    int N, target, input, sum;
-    vector<int> list;
-    scanf("%d %d", &N, &target);
-    
-    while(N--){
-        scanf("%d", &input);
-        list.push_back(input);
+int n,s;
+int cnt = 0;
+vector<int> arr(20);
+void solve(int index, int sum) {
+    if(index == n){
+        if(sum == s)
+            cnt++;
+        return;
     }
+    solve(index+1, sum+arr[index]);
+    solve(index+1, sum);
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
-    sort(list.begin(), list.end());
-    sum = 0;
+    cin >> n >> s;
 
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    solve(0,0);
+    if(s == 0) cnt--;
+    cout << cnt << '\n';
     return 0;
 }
--- Algorithm_cpp/15353/main.cpp
+++ Algorithm_cpp/15353/main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 #include <string>
-
+#include <cstring>
+#include <algorithm>
 using namespace std;
 int main() {
-    string a, b;
+    string a, b, output;
+    int x, y;
     cin >> a >> b;
-    
+    int current, next, dist;
+    if(a.length() > b.length()) {
+        //cout << "a is longer !" << '\n';
+        dist = a.length()-b.length();
+        for(int i=0; i<dist; i++) {
+            b.insert(0, "0");
+        }
+    } else if(a.length() < b.length()) {
+        //cout << "b is longer !" << '\n';
+        dist = b.length()-a.length();
+        for(int i=0; i<dist; i++) {
+            a.insert(0, "0");
+        }
+    }
+    dist = a.length();
+    //cout << "dist : " << dist << '\n';
+    //cout << "a : " << a << " b : " << b << '\n';
+    x = a[dist-1] - '0';
+    y = b[dist-1] - '0';
+
+    for(int i=1; i<=dist; i++) {
+        x = a[dist-i] - '0';
+        y = b[dist-i] - '0';
+        //printf("x : %d, y : %d, next : %d\n", x, y, next);
+        current = x+y+next;
+        output.insert(0, to_string(current%10));
+        next = current / 10;
+    }
+    if(next != 0) {
+        output.insert(0, "1");
+    }
+    cout << output << '\n';
+     /*
+     123 + 987
+     (3 + 7 + 0) / 10 --> 1 : next
+     (3 + 7 + 0) % 10 --> 0 : current
+     
+     (2 + 8 + 1) / 10 --> next
+     (2 + 8 + 1) % 10 --> current
+     */
     return 0;
 }
