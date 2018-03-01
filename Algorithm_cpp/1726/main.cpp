6798d6dc43e2842ffea774dc3c15c99d6225a12f
Jason <c3171700@uon.edu.au>
2018-03-01 15:30:59 +0900

    1426 - 혼자서 풀어보기

--- Algorithm_cpp/1726/main.cpp
+++ Algorithm_cpp/1726/main.cpp
@@ -1,62 +1,62 @@
 #include <iostream>
 #include <queue>
-#define MAX_SIZE 100
+#define EAST 1
+#define WEST 2
+#define SOUTH 3
+#define NORTH 4
+#define MAX_SIZE 101
 using namespace std;
 typedef struct _Point {
-    int r, c;
-    int dir;
+    int r, c, dir;
     int cnt;
 }Point;
-Point dept, dest;
-int n, m, ans;
-int map[MAX_SIZE+1][MAX_SIZE+1];
-bool check[MAX_SIZE+1][MAX_SIZE+1][5];
+Point Dept, Dest;
+queue<Point> q;
+int N, M, ans;
+int map[MAX_SIZE][MAX_SIZE];
+bool check[MAX_SIZE][MAX_SIZE][5];
 int dr[] = {0, 0, 0, 1, -1};
 int dc[] = {0, 1, -1, 0, 0};
-queue<Point> q;
-void input() {
-    cin >> n >> m;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=m; j++) {
-            cin >> map[i][j];
-        }
-    }
-    cin >> dept.r >> dept.c >> dept.dir;
-    dept.cnt = 0;
-    cin >> dest.r >> dest.c >> dest.dir;
-}
 int opp(int _dir) {
-    if(_dir == 1) return 2;
-    else if(_dir == 2) return 1;
-    else if(_dir == 3) return 4;
-    else return 3;
+    if(_dir == EAST) return WEST;
+    else if(_dir == WEST) return EAST;
+    else if(_dir == SOUTH) return NORTH;
+    else return SOUTH;
 }
-void bfs() {
-    q.push(dept);
-    check[dept.r][dept.c][dept.dir] = true;
+void input() {
+    cin >> N >> M;
+    for(int i=1; i<=N; i++)
+        for(int j=1; j<=M; j++)
+            cin >> map[i][j];
+    
+    cin >> Dept.r >> Dept.c >> Dept.dir;
+    Dept.cnt = 0;
+    cin >> Dest.r >> Dest.c >> Dest.dir;
+    q.push(Dept);
+}
+void dfs() {
     while(!q.empty()) {
         int r = q.front().r;
         int c = q.front().c;
         int dir = q.front().dir;
         int cnt = q.front().cnt;
         q.pop();
-        if(r == dest.r && c == dest.c && dir == dest.dir) {
+        
+        if(r == Dest.r && c == Dest.c && dir == Dest.dir) {
             ans = cnt;
             break;
         }
-        // 1. Go K
-        int nr, nc;
+        
         for(int i=1; i<=3; i++) {
-            nr = r + dr[dir]*i;
-            nc = c + dc[dir]*i;
-            if(nr < 1 || nr > n || nc < 1 || nc > m) break;
-            if(map[nr][nc]) break;
+            int nr = r + dr[dir]*i;
+            int nc = c + dc[dir]*i;
+            if(nr < 1 || nr > N || nc < 1 || nc > M) break;
+            if(map[nr][nc] == 1) break;
             if(check[nr][nc][dir]) continue;
             check[nr][nc][dir] = true;
             q.push(Point{nr,nc,dir,cnt+1});
         }
         
-        // 2. Turn Dir
         for(int i=1; i<=4; i++) {
             if(i == dir || i == opp(dir)) continue;
             if(check[r][c][i]) continue;
@@ -68,9 +68,8 @@ void bfs() {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie();
-    
     input();
-    bfs();
+    dfs();
     cout << ans << '\n';
     return 0;
 }
