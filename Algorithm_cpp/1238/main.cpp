424193db9151373d9679a0c1bbbaea91374570c5
Jason <c3171700@uon.edu.au>
2018-03-12 17:20:10 +0900

    1238 4% > 9%

--- Algorithm_cpp/1238/main.cpp
+++ Algorithm_cpp/1238/main.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 #include <algorithm>
 #define MAX_SIZE    1001
-#define INF         987654321
 using namespace std;
 int N, M, X, ret;
 int dist[MAX_SIZE][MAX_SIZE];
 bool visited[MAX_SIZE][MAX_SIZE]; // required ?
 int cost[MAX_SIZE];
-void init() {
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++) {
-            visited[i][j] = false;
-        }
-    }
-}
 void dfs(int dept, int dest, int current, int current_sum) {
     if(current == dest) {
         if(dist[dept][dest] == 0 || dist[dept][dest] > current_sum)
@@ -25,6 +17,7 @@ void dfs(int dept, int dest, int current, int current_sum) {
         if(current == next) continue;
         if(dist[current][next] == 0 || visited[current][next]) continue;
         if(dist[dept][dest] != 0 && current_sum + dist[current][next] > dist[dept][dest]) continue;
+        if(dist[dept][dest] != 0 && dist[next][dest] >= dist[dept][dest]) continue;
         visited[current][next] = true;
         printf(" > Current : %d\n", current);
         printf(" >> Next : %d, current_sum : %d\n", next, current_sum + dist[current][next] );
@@ -53,14 +46,12 @@ int main() {
         printf("Start : %d\n", i);
 
         dfs(i,X,i,0);
-        init();
         
         printf("For reverse : %d\n", i);
 
         dfs(X,i,X,0);
         cost[i] = dist[i][X] + dist[X][i];
         printf("Cost[%d] = %d\n", i, cost[i]);
-        init();
     }
     cout << *max_element(cost, cost+MAX_SIZE) << '\n';
     return 0;
