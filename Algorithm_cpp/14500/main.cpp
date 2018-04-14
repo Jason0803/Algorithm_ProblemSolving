1ff2cfcaed8d3a9df1978870a9956714cb3485d6
Jason <c3171700@uon.edu.au>
2018-04-14 17:32:22 +0900

    14500 - 테트로미노 - 클리어 !!

--- Algorithm_cpp/14500/main.cpp
+++ Algorithm_cpp/14500/main.cpp
@@ -1,15 +1,61 @@
-//
-//  main.cpp
-//  14500
-//
-//  Created by Soo Young Choi on 4/14/18.
-//  Copyright © 2018 Jason Choi. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int N, M, map[501][501], ans;
+bool visited[501][501];
+int dr[] = {0, 0, 1, -1};
+int dc[] = {1, -1, 0, 0};
+void exceptionalCase(int r, int c) {
+    // ㅗ
+    if(r >= 1 && c >= 1 && c < M-1) {
+        ans = max(ans, map[r][c] + map[r-1][c] + map[r][c-1] + map[r][c+1]);
+    }
+    // ㅜ
+    if(r < N-1 && c >= 1 && c < M-1) {
+        ans = max(ans, map[r][c] + map[r+1][c] + map[r][c-1] + map[r][c+1]);
+    }
+    // ㅓ
+    if(r >= 1 && c >= 1 && r < N-1) {
+        ans = max(ans, map[r][c] + map[r+1][c] + map[r-1][c] + map[r][c-1]);
+    }
+    // ㅏ
+    if(r >= 1 && r < N-1 &&c < M-1) {
+        ans = max(ans, map[r][c] + map[r-1][c] + map[r+1][c] + map[r][c+1]);
+    }
+}
+void dfs(int r, int c, int depth, int sum) {
+    if(depth == 4) {
+        ans = sum > ans ? sum : ans;
+        return;
+    }
+    for(int d=0; d<4; d++) {
+        int nr = r + dr[d];
+        int nc = c + dc[d];
+        if(nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc]) continue;
+        visited[nr][nc] = true;
+        dfs(nr, nc, depth+1, sum + map[nr][nc]);
+        visited[nr][nc] = false;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> M;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            cin >> map[i][j];
+        }
+    }
+    
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            exceptionalCase(i,j);
+            visited[i][j] = true;
+            dfs(i, j, 1, map[i][j]);
+            visited[i][j] = false;
+        }
+    }
+    cout << ans << '\n';
+    
     return 0;
 }
